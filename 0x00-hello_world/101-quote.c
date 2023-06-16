1a08f228864ae2398a424639568fbc91876c6865
nessy254 <mukaminessy9@gmail.com>
2023-06-15 19:09:40 -0700

    alx low level programming tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - beginning of program executiom
+ * Return: 1 indicates program was executed successfully
+ * */
+int main(void)
+{
+	write("2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 56);
+	return (1);
+}
