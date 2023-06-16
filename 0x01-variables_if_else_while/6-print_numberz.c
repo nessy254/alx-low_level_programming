5c1bfdff2ab91c2c583edb7e2ebe10248915a1d6
nessy254 <mukaminessy9@gmail.com>
2023-06-16 12:32:45 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ * Return: 0 if executed successfully
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 0; number++)
+	{
+		putchar(number + '0');
+	}
+	putchar("\n");
+	return (0);
+}
+
