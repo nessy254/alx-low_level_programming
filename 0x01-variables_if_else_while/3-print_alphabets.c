cbde07467c8da04a623e89eeb0b43fbbc38658fe
nessy254 <mukaminessy9@gmail.com>
2023-06-16 11:48:16 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ * Return: if successful returns zeri
+ */
+int main(void)
+{
+	for (char n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	};
+	for (char n = 'A'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
