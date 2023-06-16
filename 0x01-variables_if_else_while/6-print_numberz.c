d3e46424fc54c0a80cfef787c7f8e738a1485ef2
nessy254 <mukaminessy9@gmail.com>
2023-06-16 12:36:21 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 0; number++)
+	for (number = 0; number < 10; number++)
 	{
 		putchar(number + '0');
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
 
