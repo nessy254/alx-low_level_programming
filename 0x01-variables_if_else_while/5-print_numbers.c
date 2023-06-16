7e9fa934cdad53cde75756532e571a13ba0b033b
nessy254 <mukaminessy9@gmail.com>
2023-06-16 12:12:24 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - start of the code
+ * Return: if successful returns zero
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (1 < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	return (0);
+}
