9f39275ca33d79dc3e763d49f666302f34ce9db9
nessy254 <mukaminessy9@gmail.com>
2023-06-20 05:23:46 -0700

    task zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - start of the program
+ * Return: 0 if executed successfully
+ */
+
+int main(void)
+{
+	char s[9] = "_putchar";
+
+int i = 0;
+	
+	while (i <8)
+{
+		_putchar(s[i]);
+		i++;
+}
+	_putchar('\n');
+
+	return (0);
+}
+
