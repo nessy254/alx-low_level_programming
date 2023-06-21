e0cbf7412b5bb022b44ec037148403b3965021df
nessy254 <mukaminessy9@gmail.com>
2023-06-21 16:00:03 -0700

    task 15

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ * Description: The program that finds and prints the first
+ *98 Fibonacci numbers, from 1 and 2 then new line
+ *
+ * Return: o if successful
+ */
+int main(void)
+{
+	long int i, a1 = 1, a2 = 2, sum = 0;
+
+	for (i = 1; i < 98; i++)
+	{
+		printf("%ld, ", a1);
+		sum = a1 + a2;
+		a1 = a2;
+		a2 = sum;
+		if (i == 97)
+		{
+			printf("%ld\n", a1);
+		}
+	}
+}
