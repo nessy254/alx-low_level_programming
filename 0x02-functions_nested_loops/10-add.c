a2755a1392c02cdbd8c7ded79d55481f7919e16a
nessy254 <mukaminessy9@gmail.com>
2023-06-21 09:50:34 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - finds the sum of two integers
+ *
+ * Description: The functions computes the su of two integers
+ *
+ * @a: value of a
+ *
+ * @b: value of b
+ *
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
