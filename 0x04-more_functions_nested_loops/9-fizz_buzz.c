ab2d0672580b4a083e9bdc8ddd12e5d383b9ba67
nessy254 <mukaminessy9@gmail.com>
2023-06-22 12:34:18 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -22,6 +18,10 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
+		else if (i % 3 == 0 && i % 5 == 0)
+                {
+                        printf("FizzBuzz ");
+                }
 		else
 		{
 		printf("%d ", i);
