841520d67e7c2bfcda72b9a11d21e92a1dd64489
nessy254 <mukaminessy9@gmail.com>
2023-06-26 07:23:14 -0700

    function that takes pointer to an int as paraemeter and updates value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *reset_to_98 - resets the value to 98
- *Return: void
+ * reset_to_98 - resets the value to 98
+ * @n: integer to be passed
+ * Return: void
  */
 
 void reset_to_98(int *n)
