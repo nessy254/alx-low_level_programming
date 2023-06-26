94f6118e8abedec3616951eaf6c6691ea84a8daf
nessy254 <mukaminessy9@gmail.com>
2023-06-26 14:21:04 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,10 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	char *pontr = dest;
 
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
-	return (0);
+	return (pontr);
 }
