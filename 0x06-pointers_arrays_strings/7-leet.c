3bc294c7a36c5fa1d4688415292b90a94aa6476b
nessy254 <mukaminessy9@gmail.com>
2023-06-29 15:46:38 -0700

    function that encodes string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encodes string
+ * @str: string to be encoded
+ * Return: pointer
+ */
+char *leet(char *str)
+{
+	int a, b;
+
+	char encode[] = "aAeEoOtTlL";
+	char decode[] = "4433007711";
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (encode[b] ==str[a])
+				str[a] = decode[b];
+		}
+	}
+	return (str);
+}
