550d1f286de3377d8e3c9a68a4468f2cfb816f36
nessy254 <mukaminessy9@gmail.com>
2023-06-29 16:06:15 -0700

    encodes string using rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes string using rot13
+ * @s: string
+ * Return: s, pointer
+ */
+char *rot13(char *s)
+{
+	int m, n;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (n = 0; a[n] != '\0'; n++)
+		{
+			if(s[m] == a[n])
+			{
+				s[m] = b[n];
+				break;
+			}
+		}
+	}
+	return (s);
+}
