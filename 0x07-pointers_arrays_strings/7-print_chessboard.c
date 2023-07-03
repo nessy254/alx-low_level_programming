d596bbdcf7bf855b3af0d6bd4bc5d50e81709707
nessy254 <mukaminessy9@gmail.com>
2023-07-03 14:42:55 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[m][n]);
+		_putchar(10);
+	}
+}
