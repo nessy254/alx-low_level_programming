306ddc270c908a0acbb8a953d47a927475bfd164
nessy254 <mukaminessy9@gmail.com>
2023-07-07 15:12:24 -0700

    prints its name followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
  * main - prints its name folllowed by a new line
- * argc: count arg
- * argv: vector arg
+ * @argc: count arg
+ * @argv: vector arg
  * Return: 0
  */
-int main(int argc __attribute__((unused)) , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
