f90bc20dc65354fc92adfe0aef9a67b5ddee345f
nessy254 <mukaminessy9@gmail.com>
2023-07-27 21:23:15 +0000

    function that prints a message before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+void before_main_message(void) __attribute__((constructor));
+/**
+ * before_main_message - prints "You're beat! and yet,
+ * you must allow,\nI bore my house upon my back!\n"
+ *
+ */
+void before_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
